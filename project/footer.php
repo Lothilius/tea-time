defaddee63f58a864affa8a6b9bd8ccb2a0c782c
Lothilius <mars110110@aol.com>
2014-12-23 22:15:01 -0700

    Make changes to footer

--- project/footer.php
+++ project/footer.php
@@ -4,10 +4,9 @@ echo '
 <div class="footer">
     <ul>
         <li></li>
-        <li><a href="./contact.php">Contact Us</a></li>
-        <li><div>&copy; Treff 2013</div></li>
-        <li><div>Last edited SAM Nov. 25, 2013</div></li>
-
+        <li><a href="./error.php">Contact Us</a></li>
+        <li><div>&copy; Lothilius 2014</div></li>
+        <li><div>Last edited Lothilius Dec. 24, 2014</div></li>
     </ul>
 </div>
 
