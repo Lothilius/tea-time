14629a2e26e8bf5fe9603bef9556e67782e9c9ab
Lothilius <mars110110@aol.com>
2014-12-29 13:03:55 -0700

    Change treff file for journaling.

--- project/treff.php
+++ project/treff.php
@@ -8,67 +8,48 @@ include_once 'functions.php';
 createHeader(array("style.css"), array());
 
 $connection = connectMySql();
-//var_dump("SELECT meetingId, userId, startingStreet, startingCity, startingState, startingZip, startingCountry FROM MeetingUsers WHERE idHash='$id'");
-$result = $connection->query("SELECT meetingId, userId, startingStreet, startingCity, startingState, startingZip, startingCountry FROM MeetingUsers WHERE idHash='$id'");
+$result = $connection->query("SELECT entry_Id, user_Id, title, subject, rating, description, DATE_FORMAT(datetime, '%c-%d-%Y') AS entry_date
+                                  FROM Entries
+                                  WHERE entry_Id='$entry_Id'
+                                  AND
+                                  user_Id='".$_SESSION['user_Id']."'");
 
 $row = $result->fetch_assoc();
-$meetingId = $row['meetingId'];
-$userId = $row['userId'];
-$startingAddress = $row['startingStreet'] . ", " . $row['startingCity'] . ", " . $row['startingState'] . " " . $row['startingZip'] . ", " . $row['startingCountry'];
+$user_Id = $row['user_Id'];
+$title = $row['title'];
+$subject = $row['subject'];
 $rating = get_rating($row['rating']);
+$description = $row['description'];
+$date = $row['entry_date'];
 $result->free();
-
-$result = $connection->query("SELECT midpointStreet, midpointCity, midpointState, midpointZip, midpointCountry, midpointName, status
-                              FROM Meetings
-                              WHERE meetingId=$meetingId");
-
-$row = $result->fetch_assoc();
-$midpointAddress = $row['midpointStreet'] . ", " . $row['midpointCity'] . ", " . $row['midpointState'] . " " . $row['midpointZip'] . ", " . $row['midpointCountry'];
-$locName = $row['midpointName'];
-$status = $row['status'];
-$result->free();
-
-if ($status == 'Ready') {
-    echo '
-    <script type="text/javascript">
-        setMapCenterFromAddress(\'' . $midpointAddress . '\', true, \'' . addslashes($locName) . '\');
-    </script>';
-} else {
-    echo '
-    <script type="text/javascript">
-        setMapCenterFromAddress(\'2315 Speedway, Austin, TX 78712-1512, United States\', false);
-    </script>';
-}
-
 
 echo '
 <div class="main_body">
     <div class="infoTreff">
-        <div class="status">
-            <h1>Status: ' . $status . '</h1>
+            <h1>'.$title.'</h1>
             <table>
                 <tr>
-                    <th>User</th>
-                    <th>Status</th>
-                    <th>Reminder</th>
+                    <th>Subject:</th>'.
+                    '<td>'.$subject.'</td>'.'
+                </tr>
+                <tr>
+                    <th>Date:</th>'.
+                    '<td>'.$date.'</td>'.'
+                </tr>
+                <tr>
+                    <th>How you felt:</th>'.
+                    '<td>'.$rating.'</td>'.'
+                </tr>
+                <tr>
+                    <th>How you remember it...</th>'.'
                 </tr>';
 
 
-while($row = $result->fetch_assoc()) {
-    echo '<tr>
-              <td>' . $row['email'] . '</td>
-              <td>' . ($row['hasConfirmed'] == '1' ? 'Confirmed' : 'Not Confirmed') . '</td>';
-
-    if ($row['hasConfirmed'] == '0') {
-        echo '<td><button type="button" onclick="sendReminderEmail(' .$row['email'] . ');">Send Reminder Email</button>';
-    } else {
-        echo '<td></td>';
-    }
+    echo '<td></td>';
 
     echo '</tr>';
-}
 
-$result->free();
+echo '      </table>';
 
 echo $description .'
 	</div>
