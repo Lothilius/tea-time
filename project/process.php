5498082e2d823186080e8c3dad6ba4caf6eeeb8f
Lothilius <mars110110@aol.com>
2014-12-29 13:00:02 -0700

    Remove unnecessary references and change use for journal

--- project/process.php
+++ project/process.php
@@ -12,60 +12,32 @@ if (!isset($_SESSION['user_Id']) || $_SESSION['user_Id'] == 0) {
 
 $connect = connectMySql();
 
-// Create entry in Meetings Table
-$connect->query("INSERT INTO Meetings (name)
-                 VALUES ('" . $_POST['treffName'] . "')");
-
-$meetingId = $connect->insert_id;
-
-// Create a userId for the invitee
-// But first, check if their email is already registered
-$result = $connect->query("SELECT userId FROM Users WHERE email='" . $_POST['treffMateEmail'] . "'");
-
-if ($result->num_rows > 0) {
-    $mateUserId = $result->fetch_assoc();
-    $mateUserId = $mateUserId['userId'];
-} else {
-    $connect->query("INSERT INTO Users (email, anonymous) VALUES('" . $_POST['treffMateEmail'] . "', TRUE);");
-
-    $mateUserId = $connect->insert_id;
-}
+$entry_Id = $connect->insert_id;
 
 // Create the idHashes
 $creatorIdHash = md5($meetingId . $_SESSION['user_Id']);
 $mateIdHash = md5($meetingId . $mateUserId);
 
-// Create entries in MeetingUsers table
-$connect->query("INSERT INTO MeetingUsers (idHash, meetingId, userId, startingStreet, startingCity, startingState, startingZip, startingCountry, confirmed)
-                 VALUES ('" . $creatorIdHash. "', " . $meetingId . ", " . $_SESSION['userId'] . ", '" . $_POST['street'] . "', '" . $_POST['city'] . "', '" . $_POST['state'] . "', '" . $_POST['zip'] . "', '" . $_POST['country'] . "', 1),
-                        ('" . $mateIdHash. "', " . $meetingId . ", " . $mateUserId . ", NULL, NULL, NULL, NULL, NULL, 0)");
+//print_r("<pre>".date('Y-m-d H:i:s', time())."</pre>");
+// Create journal entry in Entries table
+$connect->query("INSERT INTO Entries (user_Id, title, subject, rating, description, datetime)
+                 VALUES ('" . $_SESSION['user_Id']. "', '" . $_POST['title'] . "', '" . $_POST['subject'] . "', '" . $_POST['rating'] . "', '" . $_POST['description'] . "', '". date('Y-m-d H:i:s') . "')");
 
-// Send emails
-$mg = new Mailgun("key-3g4koukbw35jwaa0ldtd32sqjzq-7948");
-$domain = "project";
-
-
-# Send confirmation email to creator
-$mg->sendMessage($domain,
-    array('from'    => 'Treff <noreply@project>',
-          'to'      => $_POST['creatorEmail'],
-          'subject' => 'Confirmation for creating the Treff "' . $_POST['treffName'] . '"',
-          'text'    => "Thank you for using Treff! We hope your experience was simple and timely.\n\n" .
-                       "This is a confirmation email for the Treff you created. Below is a link to the meeting main page.\n" .
-                       "http://project/treff.php?idHash=$creatorIdHash\n\n" .
-                       "Happy Treffing,\n" .
-                       "The Treff Team"));
-
-# Send confirmation email to mate
-$mg->sendMessage($domain,
-    array('from'    => 'Treff <noreply@project>',
-          'to'      => $_POST['treffMateEmail'],
-          'subject' => 'Invitation to Treff "' . $_POST['treffName'] . '"',
-          'text'    => "Welcome to Treff, a service for creating meeting points between people!\n\n" .
-                       $_POST['creatorEmail'] . " has invited you to their Treff. Below is a link to the meeting main page.\n" .
-                       "http://project/jointreff.php?idHash=$mateIdHash\n\n" .
-                       "Happy Treffing,\n" .
-                       "The Treff Team"));
+//// Send emails
+//$mg = new Mailgun("key-3g4koukbw35jwaa0ldtd32sqjzq-7948");
+//$domain = "project";
+//
+//
+//# Send confirmation email to creator
+//$mg->sendMessage($domain,
+//    array('from'    => 'Treff <noreply@project>',
+//          'to'      => 'mars110110@aol.com',
+//          'subject' => 'Confirmation for creating the Treff "' . $_POST['treffName'] . '"',
+//          'text'    => "Thank you for using Treff! We hope your experience was simple and timely.\n\n" .
+//                       "This is a confirmation email for the Treff you created. Below is a link to the meeting main page.\n" .
+//                       "http://project/treff.php?idHash=$creatorIdHash\n\n" .
+//                       "Happy Treffing,\n" .
+//                       "The Treff Team"));
 
 
-header("Location: treff.php?idHash=$creatorIdHash");
+header("Location: /view_entries.php");
